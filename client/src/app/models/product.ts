45da31bca61cd23c79d720594497bb0c9487c5c6
jonirafael <jontel@icloud.com>
2023-10-07 13:55:45 +0700

    End of section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: number;
+    productCode: string;
+    name: string;
+    uom: string;
+    description: string;
+    hnappn: number;
+    pictureURL: string;
+    productFactory: string;
+    productCategory: string;
+}
